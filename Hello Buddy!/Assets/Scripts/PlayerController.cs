619a4fe5add58e1220e203a43d7f5f217cb99c5b
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2019-10-05 11:36:54 +0200

    Added basic player movement

---
+++ Hello Buddy!/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float left = (Input.GetKey("a") ? -1f : 0f) * speed;
+        float right = (Input.GetKey("d") ? 1f : 0f) * speed;
+        float forward = (Input.GetKey("w") ? 1f : 0f) * speed;
+        float backward = (Input.GetKey("s") ? -1f : 0f) * speed;
+
+        Vector3 forceVector = new Vector3(left + right, 0, forward + backward);
+
+        rb.AddForce(forceVector);
+    }
+}
