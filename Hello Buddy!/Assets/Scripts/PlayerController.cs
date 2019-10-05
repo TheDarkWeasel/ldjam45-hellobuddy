3ba12eddae6a495c9a175546697596cbbce68cd4
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2019-10-05 12:20:02 +0200

    Added rotation an more character details ;)

--- Hello Buddy!/Assets/Scripts/PlayerController.cs
+++ Hello Buddy!/Assets/Scripts/PlayerController.cs
@@ -10,14 +10,25 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+
+    void Update()
+    {
+        float rotationLeft = (Input.GetKey("q") ? -1f : 0f) * speed;
+        float rotationRight = (Input.GetKey("e") ? 1f : 0f) * speed;
+
+        Vector3 rotationVector = new Vector3(0, rotationLeft + rotationRight, 0);
+
+        gameObject.transform.Rotate(rotationVector);
+    }
+
     void FixedUpdate()
     {
         float left = (Input.GetKey("a") ? -1f : 0f) * speed;
         float right = (Input.GetKey("d") ? 1f : 0f) * speed;
-        float forward = (Input.GetKey("w") ? 1f : 0f) * speed;
-        float backward = (Input.GetKey("s") ? -1f : 0f) * speed;
+        //float forward = (Input.GetKey("w") ? 1f : 0f) * speed;
+        //float backward = (Input.GetKey("s") ? -1f : 0f) * speed;
 
-        Vector3 forceVector = new Vector3(left + right, 0, forward + backward);
+        Vector3 forceVector = new Vector3(left + right, 0, 0);
 
         rb.AddForce(forceVector);
     }
