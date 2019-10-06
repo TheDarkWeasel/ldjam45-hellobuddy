b9be7fad24074f10eaab701096bf4dbd88faba4b
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2019-10-06 14:42:40 +0200

    After load the game after the title

---
+++ Hello Buddy!/Assets/Scripts/StartscreenManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartscreenManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
